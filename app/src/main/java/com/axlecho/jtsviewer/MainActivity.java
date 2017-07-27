2707b68c4d7a4fa82a5634fda68f8fa37b297cfc
d39 <axlecho@gmail.com>
2017-07-27 17:31:08 +0800

    fix open second time crash

--- app/src/main/java/com/axlecho/jtsviewer/MainActivity.java
+++ app/src/main/java/com/axlecho/jtsviewer/MainActivity.java
@@ -41,13 +41,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
-        } else {
-            Intent openCameraIntent = new Intent(this, MainActivity.class);
-            startActivityForResult(openCameraIntent, 0);
-        }
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
