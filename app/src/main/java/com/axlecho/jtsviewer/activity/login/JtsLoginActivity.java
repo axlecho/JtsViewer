685ff7376f646fe5837f3b370acaa67ccf81162a
axlecho <axlecho.gmail.com>
2018-01-02 14:04:24 +0800

    [feture] improve the login page

--- app/src/main/java/com/axlecho/jtsviewer/activity/login/JtsLoginActivity.java
+++ app/src/main/java/com/axlecho/jtsviewer/activity/login/JtsLoginActivity.java
@@ -1,12 +1,14 @@
 package com.axlecho.jtsviewer.activity.login;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import com.axlecho.jtsviewer.R;
@@ -14,6 +16,10 @@ import com.axlecho.jtsviewer.network.JtsServer;
 import com.axlecho.jtsviewer.untils.JtsConf;
 import com.axlecho.jtsviewer.untils.JtsToolUnitls;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 
 public class JtsLoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -42,6 +48,19 @@ public class JtsLoginActivity extends AppCompatActivity implements View.OnClickL
         usernameEditText = (EditText) findViewById(R.id.login_username_edittext);
         passwordEditText = (EditText) findViewById(R.id.login_password_edittext);
         loadingProgressDialog = new ProgressDialog(this);
+        Observable.timer(400, TimeUnit.MILLISECONDS)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        showInputMethod();
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        showError(throwable.getMessage());
+                    }
+                });
     }
 
     @Override
@@ -75,6 +94,14 @@ public class JtsLoginActivity extends AppCompatActivity implements View.OnClickL
                         showError(throwable.getMessage());
                     }
                 });
+    }
+
+    private void showInputMethod() {
+        usernameEditText.setFocusable(true);
+        usernameEditText.setFocusableInTouchMode(true);
+        usernameEditText.requestFocus();
+
+        JtsToolUnitls.showSoftInput(this, usernameEditText);
     }
 
     private void showResult(boolean status) {
--- app/src/main/java/com/axlecho/jtsviewer/untils/JtsToolUnitls.java
+++ app/src/main/java/com/axlecho/jtsviewer/untils/JtsToolUnitls.java
@@ -19,6 +19,13 @@ public class JtsToolUnitls {
         imm.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
+    public static void showSoftInput(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm == null) {
+            return;
+        }
+        imm.showSoftInput(view, 0);
+    }
 
     public static void openUrl(Context context, String url) {
         Intent intent = new Intent();
